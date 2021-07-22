3d66a52b4e103ca29c5f09d123735f962f67cc3a
mustafaev05 <shamil.mustafaev@norse.digital>
2021-07-22 16:31:58 +0300

    added controller tests

--- src/main/java/com/searchengine/controller/FileUploadController.java
+++ src/main/java/com/searchengine/controller/FileUploadController.java
@@ -2,6 +2,8 @@ package com.searchengine.controller;
 
 import com.searchengine.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +20,10 @@ public class FileUploadController {
     FileService fileUploadService;
 
     @PostMapping("/uploadFile")
-    public void uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
         fileUploadService.uploadFile(file);
+        return file.isEmpty() ?
+                new ResponseEntity<String>(HttpStatus.NOT_FOUND) : new ResponseEntity<String>(HttpStatus.OK);
     }
 
     @GetMapping("/search")
